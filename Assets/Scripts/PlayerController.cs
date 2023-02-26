6173bc8a4aa3974a1a726c5aed377e5f8d73a660
Adriaan <adriaan.fazant@live.nl>
2023-02-26 19:27:06 +0100

    Player Interaction Done
    Description:
        When the "Interact" button is pressed, a raycast is created from the camera that checks if the layer of the hit object is in the interactables layermask. If it is it checks for the IInteractable interface. If said object contains said interface then the Interact function from said interface will be called on said object.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Movement Variables
     Vector2 moveInput;
     Vector3 moveDir;
     Vector2 mouseRotationInput, playerRotation;
@@ -15,10 +16,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     bool moveLock;
 
+    #endregion
+
+    #region Interaction Variables
+
+    public LayerMask interactableLayers;
+    public float interactionRange;
+
+    Ray targetRay;
+    RaycastHit targetRayHit;
+    #endregion
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+
+
     }
 
     void Update()
@@ -26,6 +40,14 @@ public class PlayerController : MonoBehaviour
         mouseRotationInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveDir = (transform.forward * moveInput.y + transform.right * moveInput.x).normalized;
+
+        if(Input.GetButtonDown("Interact"))
+        {
+            Interact();
+        }
+
+        Debug.DrawRay(camera.transform.position, camera.transform.forward * interactionRange, Color.red);
+
     }
 
     void FixedUpdate()
@@ -42,6 +64,24 @@ public class PlayerController : MonoBehaviour
             camera.transform.localRotation = Quaternion.Euler(transform.rotation.x + -playerRotation.y, playerRb.rotation.y, 0);
 
             #endregion
+        }
+    }
+
+    void Interact()
+    {
+        targetRay = new Ray(camera.transform.position, camera.transform.forward);
+        targetRayHit = new RaycastHit();
+
+        if (Physics.Raycast(targetRay, out targetRayHit, interactionRange, interactableLayers))
+        {
+            var interactableObject = targetRayHit.transform.gameObject.GetComponent<IInteractable>();
+            if (interactableObject != null)
+            {
+                interactableObject.Interact();
+            }
+            else
+                Debug.Log("This object cannot be interacted with");
+            
         }
     }
 }
