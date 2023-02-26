8cebac022cf689ee75271722ff961efb363d5482
Adriaan <adriaan.fazant@live.nl>
2023-02-26 18:25:56 +0100

    Movement & Rotation done
    Description:
        The player can move (using a rigidbody component) and turn. The camera turns with the player on the y axis but rotates independently on the x axis.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 moveInput;
+    Vector3 moveDir;
+    Vector2 mouseRotationInput, playerRotation;
+
+    public GameObject camera;
+    public Vector2 cameraAngleConstraints;
+
+    public Rigidbody playerRb;
+    public float moveSpeed;
+
+    [SerializeField]
+    bool moveLock;
+
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        mouseRotationInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveDir = (transform.forward * moveInput.y + transform.right * moveInput.x).normalized;
+
+       
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (moveLock != true)
+        {
+            playerRb.MovePosition(playerRb.position + (moveDir * moveSpeed * Time.deltaTime));
+
+            #region Camera Controls
+
+            playerRotation = new Vector2(playerRotation.x + mouseRotationInput.x, Mathf.Clamp(playerRotation.y + mouseRotationInput.y, cameraAngleConstraints.x, cameraAngleConstraints.y));
+
+            playerRb.MoveRotation(Quaternion.Euler(0, playerRotation.x, 0));
+            camera.transform.localRotation = Quaternion.Euler(transform.rotation.x + -playerRotation.y, playerRb.rotation.y, 0);
+
+            #endregion
+        }
     }
 }
